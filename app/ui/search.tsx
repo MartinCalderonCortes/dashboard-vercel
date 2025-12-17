5370db373461c6912525133cffca5dedcdbe86b1
Martin <mh.straus@hotmail.com>
2025-12-17 17:28:40 -0500

    Enable pagination functionality for the invoices module

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,14 +10,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term: string): void => {
-    const params = new URLSearchParams(searchParams)
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if(term) {
       params.set('query', term)
     } else {
       params.delete('query')
     }
     replace(`${pathname}?${params.toString()}`)
-  }, 500)
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
